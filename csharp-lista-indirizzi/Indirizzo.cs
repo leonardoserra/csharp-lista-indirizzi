be8c3c8b0373e11bbe8478d6e99124ad5952087b
leonardoserra <serra.leonardo92@gmail.com>
2023-09-14 15:05:52 +0200

    creata class Indirizzo con props e costruttore

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Indirizzo
+    {
+        public string Name{ get; private set; }
+        public string Surname{ get; private set; }
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string Province { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zipCode)
+        {
+            this.Name = name;
+            this.Surname = surname; 
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.ZipCode = zipCode;
+        }
+    }
+}
--- csharp-lista-indirizzi/README.md
+++ csharp-lista-indirizzi/README.md
@@ -2,6 +2,6 @@
 
 Oggi esercitazione sui file, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.  
 
-In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.  
+In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti nel file all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.  
 
 Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.  
